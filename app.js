98a8e5c2c1affe6b3c0b0c2d18f7c67213cada68
NiksonGlikson <hermesshant@gmail.com>
2022-04-19 10:03:52 +0300

    check mistakes1

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
-
-const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
