2f6eee5bbe464abed07cfb6ac72ae5990598aba8
NiksonGlikson <hermesshant@gmail.com>
2022-05-06 18:05:01 +0300

    fixed mistakes

--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,14 @@ const {
   getMe,
 } = require("../controllers/users");
 
+router.get("/me", getMe);
 router.get("/", getUsers);
 router.get("/:userId", celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).hex().required(),
   }),
 }), getUser);
-router.get("/me", getMe);
+
 router.patch("/me/avatar", celebrate({
   body: Joi.object().keys({
     avatar: Joi.string()
