f948680206d29c01fe56301aaf53ec8f1405e869
NiksonGlikson <hermesshant@gmail.com>
2022-05-19 00:46:15 +0300

    Revert "update"
    This reverts commit 82f0d901e3ba5c23271b3dcda439c1d635101fac.

--- app.js
+++ app.js
@@ -27,34 +27,6 @@ const auth = require("./middlewares/auth");
 const CatcherError = require("./errors/CatcherError");
 const NotFoundError = require("./errors/NotFoundError");
 
-// Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  "https://praktikum.tk",
-  "http://praktikum.tk",
-  "localhost:3000"
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", "*");
-  }
-
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE"; // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const requestHeaders = req.headers["access-control-request-headers"]; // сохраняем список заголовков исходного запроса
-
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === "OPTIONS") {
-  // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-    res.header("Access-Control-Allow-Headers", requestHeaders);
-    return res.end();
-  }
-  return (next);
-});
-
 app.use(requestLogger);
 
 app.post("/signin", celebrate({
